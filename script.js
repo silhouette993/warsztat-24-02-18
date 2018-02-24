9f06a5ec378a0bb79976200086c785b01504089c
Sylwia Krakowiak <krakowiaksylwia@gmail.com>
2018-02-24 12:50:28 +0100

    Data types - objects

--- script.js
+++ script.js
@@ -24,4 +24,35 @@ console.log(nullVariable)
 var undefinedVariable = undefined
 console.log(undefinedVariable)
 
+// objects
+
+
+// objects with data
+var simplestObj = {
+    name: "Sylwia",
+    lastname: "Krakowiak"
+}
+console.log(simplestObj)
+
+// objects with data
+var nastedObj = {
+    name: "Sylwia",
+    car: {
+        brand: "Peugeot",
+        model: 407 
+    }
+}
+console.log(nastedObj)
+console.log(nastedObj.car.model)
+console.log(nastedObj.car.year)
+
+// arrays
+var simpleArray = [1,
+    {name: "Sylwia", array: [1,4,6]},
+    3,
+    4,
+    5]
+console.log(simpleArray)
+console.log(simpleArray[4])
+console.log(simpleArray[1].name)
 
