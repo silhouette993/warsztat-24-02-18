08d51b85e91be12a68f020744156becac3fccc3d
Sylwia Krakowiak <krakowiaksylwia@gmail.com>
2018-02-24 15:36:41 +0100

    kret latający

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
-var body = document.querySelector('body')
+(function (){
 
-console.log(body)
+    function makeMole (){
+        var molePosX = Math.round(
+            Math.random() * (window.innerWidth - window.innerHeight /10)
+        )
+        var molePosY = Math.round(
+            Math.random() * (window.innerHeight - window.innerHeight /10)
+        )
 
-body.addEventListener(
-    'click',
-    function(){
-        console.log('Klik!')
-    },
-)
+        var mole = document.createElement ('div')
+
+        mole.classList.add('mole')
+
+        mole.style.left = molePosX + 'px'
+        mole.style.top = molePosY + 'px'
+
+        document.querySelector('body').appendChild(mole)
+    }
+
+    makeMole()
+
+}) ()
