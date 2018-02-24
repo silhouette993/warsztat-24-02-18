eae7045c3f943f291ea072fcd257a7b08b215681
Sylwia Krakowiak <krakowiaksylwia@gmail.com>
2018-02-24 12:28:32 +0100

    Data types - primitives

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 console.log('Hello Sylwia!')
+
+// data types in JS
+
+// primitives
+
+// string
+var textVariable = "Sylwia"
+console.log(textVariable)
+
+// numer
+var numberVariable = 43
+console.log(numberVariable)
+
+// boolean
+var boolVariable = true
+console.log(boolVariable)
+
+// null
+var nullVariable = null
+console.log(nullVariable)
+
+//undefined
+var undefinedVariable = undefined
+console.log(undefinedVariable)
+
+
